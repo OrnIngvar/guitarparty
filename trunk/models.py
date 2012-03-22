26a0294a1dbfcf8d4b8317d1b664da8f76c4c211
orn@skynvilla.is <orn@skynvilla.is@3729f947-16a3-1b0b-bd0e-f5f099f3d890>
2012-03-22 18:32:43 +0000

    upload í grunn farið að virka

--- trunk/models.py
+++ trunk/models.py
@@ -7,5 +7,7 @@ class Song(db.Model):
     title = TextField()
     artist = TextField()
     album = TextField()
+    path = TextField()
     created = DateTimeField(default=datetime.datetime.now)
+
 
