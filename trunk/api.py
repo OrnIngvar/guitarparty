68d046d825d466428ac6262253190f143fbc531a
orn@skynvilla.is <orn@skynvilla.is@3729f947-16a3-1b0b-bd0e-f5f099f3d890>
2012-03-24 13:35:21 +0000

    commentaði api functions og queries. bætti við view sem sendir uploaded file

--- trunk/api.py
+++ trunk/api.py
@@ -1,4 +1,4 @@
-from flask_peewee.rest import RestAPI, RestResource, UserAuthentication, AdminAuthentication, RestrictOwnerResource
+from flask_peewee.rest import RestAPI, UserAuthentication
 
 from app import app
 from auth import auth
@@ -11,3 +11,22 @@ api = RestAPI(app, default_auth=user_auth)
 
 api.register(Song)
 
+# get all songs : /api/song
+# get a single song : /api/song/<id>
+
+# upload a song : /api/song/upload
+
+# filter by artist name : /api/song/?artist=Mugison
+# filter by song title : /api/song/?title=Stingum af
+# or
+# /api/song/?title__eq=Stingum+af
+
+# filter by album name : /api/song/?album=Mugiboogie
+
+# filter by artist starts with : /api/song/?artist__istartswith=Mugi
+# filter by artist name contains : /api/song/?artist__icontains=Mug
+
+# sort by created date desc : /api/song/?genre__eq=Rock&ordering=created
+# sort by created date desc : /api/song/?genre__eq=Rock&ordering=-created
+
+# limit results : /api/song/?limit=1
